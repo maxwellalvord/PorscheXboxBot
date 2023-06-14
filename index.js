9f8e6e5377ad03a9daa39009a7e3fd286738f18a
Maxwell Alvord <maxwellalvord@gmail.com>
2023-06-14 15:01:30 -0700

    WIP index.js: found solution for checkbox, cloudflare blocking selenium, may restart using python to use a trusted undetected-chromedriver package.

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ async function fill_form(){
   await driver.findElement(By.id("fname")).sendKeys("Maxwell");
   await driver.findElement(By.id("lname")).sendKeys("Alvord");
   await driver.findElement(By.id("email")).sendKeys("maxwellalvord@gmail.com");
-  // await driver.findElement(By.localName("::after")).click();
+  await driver.findElement(By.xpath("//label[@for='opt_rules']")).click();
 
 
-  // await driver.findElement(By.innerHTML("SUBMIT")).click();
+  await driver.findElement(By.className("btn btn-primary d-block w-100")).click();
   
 
   console.log("Success");
 
-  setInterval(function(){
-    driver.quit();
-  }, 7000);
+  // setInterval(function(){
+  //   driver.quit();
+  // }, 7000);
   
 }
 
