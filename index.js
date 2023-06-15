a799d6bd30c6b50da2943cee06323fd84b079b43
Maxwell Alvord <maxwellalvord@gmail.com>
2023-06-15 08:56:12 -0700

    WIP index.js: trying out puppeteer to avoid cloudflare verification.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const { By , Key, Builder} = require("selenium-webdriver");
 require("chromedriver");
 
+const puppeteer = require("puppeteer");
+
 async function fill_form(){
-  let driver = await new Builder().forBrowser("chrome").build();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-  await driver.get("https://www.porschexboxsweepstakes.com");
+  await page.goto('https://www.porschexboxsweepstakes.com');
 
-  await driver.findElement(By.id("fname")).sendKeys("Maxwell");
-  await driver.findElement(By.id("lname")).sendKeys("Alvord");
-  await driver.findElement(By.id("email")).sendKeys("maxwellalvord@gmail.com");
-  await driver.findElement(By.xpath("//label[@for='opt_rules']")).click();
+  await page.setViewport({width: 1080, height: 1024});
+  await page.screenshot({ path: 'test.png' });
+  await browser.close();
 
 
-  await driver.findElement(By.className("btn btn-primary d-block w-100")).click();
+  // let driver = await new Builder().forBrowser("chrome").build();
+
+  // await driver.get("https://www.porschexboxsweepstakes.com");
+
+  // await driver.findElement(By.id("fname")).sendKeys("Maxwell");
+  // await driver.findElement(By.id("lname")).sendKeys("Alvord");
+  // await driver.findElement(By.id("email")).sendKeys("maxwellalvord@gmail.com");
+  // await driver.findElement(By.xpath("//label[@for='opt_rules']")).click();
+
+
+  // await driver.findElement(By.className("btn btn-primary d-block w-100")).click();
   
 
   console.log("Success");
