d6873ad9bbee85bac843988b9aea7d01e3f2ffda
Maxwell Alvord <maxwellalvord@gmail.com>
2023-06-16 15:34:53 -0700

    WIP Index.js: fill out form with puppeteer/ still need to find puppeteer locator for checkbox.

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ require("chromedriver");
 const puppeteer = require("puppeteer");
 
 async function fill_form(){
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: './tmp'
+  });
   const page = await browser.newPage();
 
   await page.goto('https://www.porschexboxsweepstakes.com');
 
   await page.setViewport({width: 1080, height: 1024});
-  await page.screenshot({ path: 'test.png' });
-  await browser.close();
+
+  await page.type("#fname", "Maxwell");
+  await page.type("#lname", "Alvord");
+  await page.type("#email", "maxwellalvord@gmail.com");
+  // await page.screenshot({ path: 'test.png' });
+  // await browser.close();
 
 
   // let driver = await new Builder().forBrowser("chrome").build();
