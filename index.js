8be9c65f598a26eac61b0a2fb4e0754f6f21f8cd
Maxwell Alvord <maxwellalvord@gmail.com>
2023-06-20 15:26:56 -0700

    Update index.js: use stealth plugin for puppeteer to get around cloud flare anti bot.

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
-const { By , Key, Builder} = require("selenium-webdriver");
-require("chromedriver");
 
-const puppeteer = require("puppeteer");
+
+const puppeteer = require("puppeteer-extra");
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 async function fill_form(){
   const browser = await puppeteer.launch({
-    headless: false,
-    userDataDir: './tmp'
+    headless: false
   });
   const page = await browser.newPage();
 
   await page.goto('https://www.porschexboxsweepstakes.com');
 
-  await page.setViewport({width: 1080, height: 1024});
-
   await page.type("#fname", "Maxwell");
   await page.type("#lname", "Alvord");
   await page.type("#email", "maxwellalvord@gmail.com");
   await page.locator('.form-check label').click();
-  // await page.screenshot({ path: 'test.png' });
-  // await browser.close();
-
-
-  // let driver = await new Builder().forBrowser("chrome").build();
-
-  // await driver.get("https://www.porschexboxsweepstakes.com");
-
-  // await driver.findElement(By.id("fname")).sendKeys("Maxwell");
-  // await driver.findElement(By.id("lname")).sendKeys("Alvord");
-  // await driver.findElement(By.id("email")).sendKeys("maxwellalvord@gmail.com");
-  // await driver.findElement(By.xpath("//label[@for='opt_rules']")).click();
-
-
-  // await driver.findElement(By.className("btn btn-primary d-block w-100")).click();
-  
-
+  await page.locator('.btn.btn-primary').click();
   console.log("Success");
-
-  // setInterval(function(){
-  //   driver.quit();
-  // }, 7000);
+  browser.close();
+ 
   
 }
 
