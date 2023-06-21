78aa39c9dcdf4720c7efd12e0b3e67a79b421eeb
Maxwell Alvord <maxwellalvord@gmail.com>
2023-06-21 08:32:14 -0700

    Update: clean up package and gitignore file as well as add a clear interval to clean up some errors.

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ puppeteer.use(StealthPlugin())
 
 async function fill_form(){
   const browser = await puppeteer.launch({
-    headless: true
+    headless: false
   });
   const page = await browser.newPage();
+
+  let timesRun = 0;
 
   await page.goto('https://www.porschexboxsweepstakes.com');
 
@@ -17,13 +19,18 @@ async function fill_form(){
   await page.type("#lname", "Alvord");
   await page.type("#email", "maxwellalvord@gmail.com");
   await page.locator('.form-check label').click();
-  setInterval(async function(){
+  const interval = setInterval(async function(){
     await page.locator('.btn.btn-primary').click();
     console.log('success')
+    timesRun += 1;
+    if(timesRun === 3){
+        clearInterval(interval);
+    }
   }, 300);
   setInterval(function(){
     browser.close();
-  }, 900);
+    process.kill();
+  }, 1200);
   
 }
 
